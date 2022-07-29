ce77c26cca1ec8acca820c3860f782a2b14c7659
rau1310 <rmusakhan@college-narxoz.kz>
2022-07-29 17:11:55 +0600

    ez

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        if(1 != 1){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
     }
 }
+
